332488531f6cf4bf50b1c5a4801c483ab6b195db
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-07 22:36:55 +0530

    use secret key from .env file

--- guest_house/guest_house/settings.py
+++ guest_house/guest_house/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv,dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fsa6ov)$r@-joab0+z#2995=e5h7!pt1i9773#+_6@69wwbjy#'
-
+SECRET_KEY = dotenv_values('.env').get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
