d64183852973bb444526028b7ab12672dd261c55
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-22 13:43:18 +0530

    add user registration feature

--- guest-house-frontend/components/LoginForm/LoginForm.jsx
+++ guest-house-frontend/components/LoginForm/LoginForm.jsx
@@ -1,57 +1,83 @@
 // pages/contact-us.jsx
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import "./LoginForm.css";
-
+import api from "../../api";
 export default function LoginForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  }
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log("Form data:", formData);
-  }
+    function handleSubmit(e) {
+        e.preventDefault();
+        // Handle form submission logic here
+        // For example, send the form data to the server
+        api.post("/auth/token/", formData).then((response) => {
+            data = response.data;
+            console.log("Server response:", response);
+            localStorage.setItem("access", data.access);
+            localStorage.setItem("refresh", data.refresh);
+        });
+        console.log("Form data:", formData);
+    }
 
-  return (
-    <>
-      <form className="contactContainer">
-        <div className="contactUsPage">
-          <h1>Django Rest Framework </h1>
-          <div className="contactContent">
-            <form onSubmit={handleSubmit} className="contactForm">
-              <div>
-                <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  placeholder="Username"
-                  required
-                />
-              </div>
-              <div>
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  placeholder="Password"
-                  required
-                />
-              </div>
-              <button type="submit">Log in</button>
-            </form>
-          </div>
-        </div>
-      </form>
-    </>
-  );
+    function handleRegister(e) {
+        e.preventDefault();
+      api.post("/auth/user/register/", formData)
+          .then((response) => {  
+              const { data }  = response.data;
+        //   console.log("login data:", response);
+        })
+      .error((error) => {
+        console.log("Server error:", error);
+      });
+        console.log("Form data:", formData);
+    }
+
+
+    return (
+        <>
+            <div className="contactContainer">
+                <div className="contactUsPage">
+                    <h1>Login </h1>
+                    <div className="contactContent">
+                        <form className="contactForm">
+                            <div>
+                                <input
+                                    type="email"
+                                    name="username"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                    placeholder="Username"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    placeholder="Password"
+                                    required
+                                />
+                            </div>
+                            <button type="button" onClick={handleSubmit}>
+                                Log in
+                            </button>
+                            <button type="button" onClick={handleRegister}>
+                                Register
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
--- guest_house/auth/views.py
+++ guest_house/auth/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
