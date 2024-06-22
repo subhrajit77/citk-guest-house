6b1434a1f4c73c227613ff4e106ffb784c41b73c
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-22 13:08:25 +0530

    add intercepter to axios instance

--- guest-house-frontend/api.js
+++ guest-house-frontend/api.js
@@ -8,10 +8,21 @@ const api = axios.create({
     baseURL: url,
     headers: {
         "Content-Type": "application/json",
-        
     },
 });
 
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 axios.defaults.withCredentials = true;
 
