07dcdb413533bfb4aba25a482a88da79eccab361
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-01 19:15:12 +0530

    Update settings.py

--- guest_house/guest_house/settings.py
+++ guest_house/guest_house/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = dotenv_values('.env').get('SECRET_KEY') or os.environ.get('SECRET_KEY')
+SECRET_KEY = dotenv_values('.env').get('SECRET_KEY') or os.environ.get('SECRET_KEY') or "910$k_7(2i(fz!sr-yt3%&zm(#jny"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
