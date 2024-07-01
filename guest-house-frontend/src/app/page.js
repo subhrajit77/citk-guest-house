bf5c12fdecd602e9900669230944b37a7b251f72
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-02 00:43:24 +0530

    add protected route

--- guest-house-frontend/src/app/page.js
+++ guest-house-frontend/src/app/page.js
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
-
+import ProtectedRoute from "../../components/auth/protected/ProtectedRoute";
 export default function Home() {
   return (
-    <>
+    <ProtectedRoute>
       <p>Home Page</p>
       <a href="/login">Login</a>
-    </>
+    </ProtectedRoute>
     
   );
 }
+
