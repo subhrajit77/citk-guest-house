7f8208823275cfc0c691b421c5d05be3107b4204
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-01 14:22:41 +0530

    fix env

--- guest_house/guest_house/settings.py
+++ guest_house/guest_house/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv, dotenv_values
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = dotenv_values('.env').get('SECRET_KEY')
+SECRET_KEY = dotenv_values('.env').get('SECRET_KEY') or os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
