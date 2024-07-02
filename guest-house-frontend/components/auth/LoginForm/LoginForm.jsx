be4d54c42b8131e3afbdf326cc12fc662478927f
Abhishek Kashyap <abhik5567@gmail.com>
2024-07-02 23:21:53 +0530

    use react-hook form

--- guest-house-frontend/components/auth/LoginForm/LoginForm.jsx
+++ guest-house-frontend/components/auth/LoginForm/LoginForm.jsx
@@ -1,20 +1,37 @@
 "use client";
-import { useState } from "react";
 import "./LoginForm.css";
 import api from "../../../api";
+import { useForm } from "react-hook-form";
 export default function LoginForm() {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: "",
-    });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const registerOptions = {
+        username: {
+            required: "Username is required",
+            minLength: {
+                value: 3,
+                message: "Username must be at least 3 characters long",
+            },
+        },
+        password: {
+            required: "Password is required",
+            minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+            },
+        },
+    };
 
-    function handleChange(e) {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+    // remove all error checks
+    for (let key in registerOptions) {
+        registerOptions[key] = {};
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    function handleLogin(formData) {
+        // console.log("Form data:", formData);
         api.post("/auth/user/login/", formData)
             .then((response) => {
                 const { data } = response;
@@ -26,41 +43,57 @@ export default function LoginForm() {
                 console.log("Server Error: ", error.response.data);
             });
     }
+    const styles = {
+        color: "red",
+        fontSize: "14px",
+        textAlign: "right",
+    };
 
     return (
         <>
-            <div className="contactContainer">
-                <div className="contactUsPage">
+            <main className="contactContainer">
+                <section className="contactUsPage">
                     <h1>Login </h1>
                     <div className="contactContent">
-                        <form className="contactForm">
+                        <form
+                            className="contactForm"
+                            onSubmit={handleSubmit(handleLogin)}
+                        >
                             <div>
                                 <input
-                                    type="email"
-                                    name="username"
-                                    value={formData.username}
-                                    onChange={handleChange}
+                                    {...register(
+                                        "username",
+                                        registerOptions.username
+                                    )}
+                                    // type="email"
                                     placeholder="Username"
-                                    required
                                 />
+                                {errors?.username && (
+                                    <div style={styles}>
+                                        {errors?.username.message}
+                                    </div>
+                                )}
                             </div>
                             <div>
                                 <input
+                                    {...register(
+                                        "password",
+                                        registerOptions.password
+                                    )}
                                     type="password"
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={handleChange}
                                     placeholder="Password"
-                                    required
                                 />
+                                {errors.password && (
+                                    <div style={styles}>
+                                        {errors.password.message}
+                                    </div>
+                                )}
                             </div>
-                            <button type="submit" onClick={handleSubmit}>
-                                Log in
-                            </button>
+                            <button type="submit">Log in</button>
                         </form>
                     </div>
-                </div>
-            </div>
+                </section>
+            </main>
         </>
     );
 }
