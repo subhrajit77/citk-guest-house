c62acc25e8d80d1e4f854579c290df9c7a92f914
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-09 01:06:34 +0530

    update user model

--- guest_house/accounts/serializers.py
+++ guest_house/accounts/serializers.py
@@ -5,11 +5,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "password"]
+        fields = ["id", "email", "first_name", "last_name", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
         return user
-
-
