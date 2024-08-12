de5c2c3594626b273654070c99af5e297c77959d
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-12 22:27:01 +0530

    add proper booking serializer

--- guest_house/booking/serializers.py
+++ guest_house/booking/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Booking
+from .models import Booking, Guest
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = "__all__"
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    guests = GuestSerializer(many=True)
+
     class Meta:
         model = Booking
         fields = "__all__"
+
+    def create(self, validated_data):
+        guests_data = validated_data.pop('guests')
+        guests = [Guest.objects.create(**guest_data)
+                  for guest_data in guests_data]
+        booking = Booking.objects.create(**validated_data)
+        booking.guests.set(guests)
+        return booking
