55427c739d43904eee7513ad40c8e6bace07fe0c
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-13 23:49:29 +0530

    user now doesn't need username

--- guest_house/accounts/models.py
+++ guest_house/accounts/models.py
@@ -1,6 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -8,25 +29,28 @@ class User(AbstractUser):
         ("Admin", "Admin"),
         ("User", "User"),
         ("Staff", "Staff"),
-        ("HOD", "HOD")
+        ("HOD", "HOD"),
     ]
     username = models.CharField(max_length=150, null=True, blank=True)
     email = models.EmailField(max_length=254, unique=True)
     designation = models.CharField(
-        max_length=100, blank=True, null=True, choices=designation_options)
+        max_length=100,
+        blank=True,
+        null=True,
+        choices=designation_options,
+        default="User",
+    )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "password", "first_name"]
+    REQUIRED_FIELDS = ["password", "first_name"]
 
     def __str__(self):
         return f"{self.first_name} - {self.designation}"
 
-    def save(self, *args, **kwargs):
-        if self.password:
-            self.password = make_password(self.password)
-            super().save(*args, **kwargs)
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
-        # db_table = "auth_user"
+
+    # db_table = "auth_user"
