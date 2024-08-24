0ddc387d106aaddbafe81144036f5eb948b89ccd
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-24 22:17:57 +0530

    add basic signals

--- guest_house/booking/apps.py
+++ guest_house/booking/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_save, m2m_changed, post_save
 
 
 class BookingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
+
+    def ready(self):
+        from . import signals
+        from .models import Booking
+
+        post_save.connect(signals.booking_confirmed, sender=Booking)
