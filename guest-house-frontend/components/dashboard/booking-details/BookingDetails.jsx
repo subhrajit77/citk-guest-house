9bf4f3b01b3fd4aa0319f20d689d508e7fd9bf0f
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-25 23:05:35 +0530

    fetch data from backend

--- guest-house-frontend/components/dashboard/booking-details/BookingDetails.jsx
+++ guest-house-frontend/components/dashboard/booking-details/BookingDetails.jsx
@@ -1,11 +1,41 @@
-import { Box, Heading, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
-import './BookingDetails.css'; // Import custom CSS for additional styling
+"use client";
+import {
+    Box,
+    Heading,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+} from "@chakra-ui/react";
+import "./BookingDetails.css";
+import BookingRow from "./BookingRow";
+import { useState, useEffect } from "react";
+import api from "../../../api";
 
 export default function BookingDetails({ bookings = [] }) {
+    const [bookingData, updateBookings] = useState([]);
+
+    async function fetchBookings() {
+        const response = await api.get("/booking/");
+        updateBookings(response.data);
+    }
+
+    useEffect(() => {
+        fetchBookings();
+    }, []);
+
     return (
         <Box p={4} bg="white" shadow="lg" rounded="lg" mt={8}>
-            <Heading as="h2" size="lg" mb={6} color="teal.500">Booking Details</Heading>
-            <Table variant="striped" colorScheme="teal" className="custom-table">
+            <Heading as="h2" size="lg" mb={6} color="teal.500">
+                Booking Details
+            </Heading>
+            <Table
+                variant="striped"
+                colorScheme="teal"
+                className="custom-table"
+            >
                 <Thead>
                     <Tr>
                         <Th>Booking ID</Th>
@@ -14,17 +44,15 @@ export default function BookingDetails({ bookings = [] }) {
                     </Tr>
                 </Thead>
                 <Tbody>
-                    {bookings.length > 0 ? (
-                        bookings.map((booking) => (
-                            <Tr key={booking.id} className="table-row">
-                                <Td>{booking.id}</Td>
-                                <Td>{booking.date}</Td>
-                                <Td className={`status-${booking.status.toLowerCase()}`}>{booking.status}</Td>
-                            </Tr>
+                    {bookingData.length > 0 ? (
+                        bookingData.map((booking) => (
+                            <BookingRow key={booking.id} {...booking} />
                         ))
                     ) : (
                         <Tr>
-                            <Td colSpan={3} textAlign="center">No bookings available</Td>
+                            <Td colSpan={3} textAlign="center">
+                                No bookings available
+                            </Td>
                         </Tr>
                     )}
                 </Tbody>
